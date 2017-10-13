b63b8ac55446365075c95bde46d7ada94380ce57
starFalll <fyxformal@gmail.com>
2017-10-13 16:14:20 +0800

    Add_hints&&Add_softwares

--- Linux_Init.sh
+++ Linux_Init.sh
@@ -10,9 +10,24 @@
 #
 #History:
 #2017/10/12	ACool	First release
+
 mkdir Backup
-cp /etc/apt/sources.list Backup/sources.list
-cp sources.list /etc/apt/sources.list
+
+gsettings set com.canonical.Unity.Launcher launcher-position Bottom
+
+test -f sources.list && result_0="y"
+if [ "${result_0}"=="y" ];then
+	cp /etc/apt/sources.list Backup/sources.list
+	cp sources.list /etc/apt/sources.list
+else
+	echo -e "The sources file which contains USTC sources does not exist!"
+	echo -e "Please check whether the file in the warehouse catalog is complete."
+	echo -e "(包含中科大的源文件不存在!请检查仓库目录下文件是否完整.)"
+	read -p "Coutinue?(Y/n)" yn
+	if [ "${yn}"=="n" ] || [ "${yn}"=="N" ];then
+		exit 0;
+	fi
+fi
 
 sudo apt-get autoclean
 sudo apt-get clean
@@ -20,14 +35,30 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get autoremove
 
-sudo apt-get install vim
+
+read -p "Is your system language Chinese?(y/n)" result_1
+if [ "${result_1}"=="Y" ] || [ "${result_1}"=="y" ];then
+echo -e "change directory to english,convenienting command line opration."
+echo -e "(改变中文目录为英文，方便命令行操作.)"
+sleep 4
 
 export LANG=en_US
 xdg-user-dirs-gtk-update
 export LANG=zh-CN
+fi
+
+read -p "Is your computer dual boot?(您的电脑是双系统吗)(Y/n)" result_2
+if [ "${result_2}"=="Y" ] || [ "${result_2}"=="y" ];then
+sudo apt-get install ntpdate
+sudo ntpdate time.windows.com
+sudo hwclock --localtime --systohc
+fi
+
+##Download softwares
+sudo apt-get install vim
+sudo apt-get install git
 
 wget http://cdn2.ime.sogou.com/dl/index/1491565850/sogoupinyin_2.1.0.0086_amd64.deb?st=H6Fv3RXvgGFlgWBT3xkMZw&e=1507788214&fn=sogoupinyin_2.1.0.0086_amd64.deb
-
 sudo dpkg -i sogoupinyin_2.1.0.0086_amd64.deb*
 
 sudo apt-get purge unity-webapps-common
