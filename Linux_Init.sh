8445a3371bdfb07c86c7f6b644285dcb50d59feb
starFalll <fyxformal@gmail.com>
2017-10-23 19:40:59 +0800

    Improve the user experience of the script

--- Linux_Init.sh
+++ Linux_Init.sh
@@ -25,10 +25,10 @@ if [ "${result_0}" == "y" ];then
 	sudo cp /etc/apt/sources.list Backup/sources.list
 	sudo cp sources.list /etc/apt/sources.list
 else
-	echo -e "The sources file which contains USTC sources does not exist!"
-	echo -e "Please check whether the file in the warehouse catalog is complete."
-	echo -e "(包含中科大的源文件不存在!请检查仓库目录下文件是否完整.)"
-	read -p "Coutinue?(Y/n)" yn
+	echo -e "\033[41;37m The sources file which contains USTC sources does not exist! \033[0m"
+	echo -e "\033[41;37m Please check whether the file in the warehouse catalog is complete. \033[0m"
+	echo -e "\033[41;37m (包含中科大的源文件不存在!请检查仓库目录下文件是否完整.) \033[0m"
+	read -p "\033[41;37m Coutinue?(Y/n) \033[0m" yn
 	if [ "${yn}" == "n" ] || [ "${yn}" == "N" ]; then
 		exit 0;
 	fi
@@ -41,10 +41,9 @@ sudo apt-get upgrade -y
 sudo apt-get autoremove
 
 
-read -p "Is your system language Chinese?(y/n)" result_1
-if [ "${result_1}" == "Y" ] || [ "${result_1}" == "y" ]; then
-	echo -e "change directory to english,convenienting command line opration."
-	echo -e "(改变中文目录为英文，方便命令行操作.)"
+if [ $LANG == "zh_CN.UTF-8" ]; then
+	echo -e "\033[44;37m change directory to english,convenienting command line opration. \033[0m"
+	echo -e "\033[44;37m (改变中文目录为英文，方便命令行操作.) \033[0m"
 sleep 4
 
 export LANG=en_US
@@ -52,7 +51,7 @@ xdg-user-dirs-gtk-update
 export LANG=zh-CN
 fi
 
-read -p "Is your computer dual boot?(您的电脑是双系统吗)(Y/n)" result_2
+read -p "\033[44;37m Is your computer dual boot?(您的电脑是双系统吗)(Y/n) \033[0m" result_2
 if [ "${result_2}" == "Y" ] || [ "${result_2}" == "y" ]; then
 	sudo apt-get install -y ntpdate
 	sudo ntpdate time.windows.com
@@ -63,7 +62,7 @@ fi
 sudo apt-get install -y vim
 sudo apt-get install -y openjdk-8*
 
-read -p"Download sougoupinyin?(安装搜狗拼音输入法) (Y/n) :" YN
+read -p"\033[44;37m Download sougoupinyin?(安装搜狗拼音输入法) (Y/n) : \033[0m" YN
 if [ "${YN}" == "Y" ] || [ "${YN}" == "y" ]; then
 	sudo apt-get remove -y fcitx*
 	sudo apt-get autoremove
@@ -91,11 +90,11 @@ sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor
 sudo apt-get update  
 sudo apt-get install -y indicator-sysmonitor
 indicator-sysmonitor &
-echo -e "Please read the page: https://github.com/starFalll/Ubuntu_Init/blob/master/README.md#title-bar-network-speed-monitoring-software-configuration"
-read -p "Have you followed the instructions?(您已经按照说明更改配置了吗?)(Y/n)" result_5
+echo -e "\033[44;37m Please read the page: https://github.com/starFalll/Ubuntu_Init/blob/master/README.md#title-bar-network-speed-monitoring-software-configuration \033[0m"
+read -p "\033[44;37m Have you followed the instructions?(您已经按照说明更改配置了吗?)(Y/n) \033[0m" result_5
 if [ "${result_5}" == "n" ] || [ "${result_5}" == "N" ]; then
-	echo -e "Please follow the instructions."
-	read -p "Continue?(Y/n)" result_6
+	echo -e "\033[44;37m Please follow the instructions. \033[0m"
+	read -p "\033[41;37m Continue?(Y/n) \033[0m" result_6
 	if [ "${result_6}" == "n" ] || ["${result_6}" == "N" ]; then
 		exit 0
 	fi
@@ -108,11 +107,11 @@ sudo apt-get install -y flatabulous-theme
 sudo add-apt-repository ppa:noobslab/icons
 sudo apt-get update
 sudo apt-get install -y ultra-flat-icons
-echo -e "Please read the page: https://github.com/starFalll/Ubuntu_Init/blob/master/README.md#system-landscaping"
-read -p "Have you followed the instructions?(您已经按照说明更改配置了吗?)(Y/n)" result_8
+echo -e "Pleao -e "\033[44;37m e read the page: https://github.com/starFalll/Ubuntu_Init/blob/master/README.md#system-landscaping \033[0m"
+read -p "\033[44;37m Have you followed the instructions?(您已经按照说明更改配置了吗?)(Y/n) \033[0m" result_8
 if [ "${result_8}" == "n" ] || [ "${result_8}" == "N" ]; then
-        echo -e "Please follow the instructions."
-        read -p "Continue?(Y/n)" result_9
+        echo -e "\033[44;37m Please follow the instructions. \033[0m"
+        read -p ""\033[41;37m Continue?(Y/n) \033[0m" result_9
         if [ "${result_9}" == "N" ] || ["${result_9}" == "n" ]; then
                 exit 0
         fi
@@ -135,7 +134,7 @@ if [ "${editer}" == "1" ]; then
 	sudo apt-get update
 	sudo apt-get -y install code
 	echo -e "VS code was installed successfully!"
-	echo -e "(vscode安装成功!)"
+	echo -e "\033[46;37m (vscode安装成功!) \033[0m"
 	sleep 3
 
 elif [ "${editer}" == "2" ]; then
@@ -146,17 +145,17 @@ elif [ "${editer}" == "2" ]; then
 	sudo apt-get update
 	sudo apt-get -y install sublime-text
 	echo -e "The sublime text3 was installed successfully!"
-	echo -e "(sublime安装成功!)"
+	echo -e "\033[46;37m (sublime安装成功!) \033[0m"
 	sleep 3
 else 
 	echo -e"No editor was installed!"	
-	echo -e"(没有安装编辑器!)"
+	echo -e"\033[41;37m (没有安装编辑器!) \033[0m"
 	sleep 3
 fi
 
 
 
-read -p "The configuration is complete ,Please follow the instructions after rebooting:https://github.com/starFalll/Ubuntu_Init/blob/master/README.md#sogou-pinyin-input-method-configuration(配置完成，请重启后按照以下页面配置 https://github.com/starFalll/Ubuntu_Init/blob/master/README.md#sogou-pinyin-input-method-configuration)(Y/n)" result_7
+read -p "\033[46;37m The configuration is complete ,Please follow the instructions after rebooting:https://github.com/starFalll/Ubuntu_Init/blob/master/README.md#sogou-pinyin-input-method-configuration(配置完成，请重启后按照以下页面配置 https://github.com/starFalll/Ubuntu_Init/blob/master/README.md#sogou-pinyin-input-method-configuration)(Y/n) \033[0m" result_7
 
 
 
